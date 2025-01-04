5a63a9e7ff6e0274483c82abce49cae1751a8320
Venkata <rajavaraprasadsanayila19@gmail.com>
2025-01-04 17:22:28 -0500

    Implemented model & view

--- src/main/java/com/raja/DecSpringBootWeb/HomeController.java
+++ src/main/java/com/raja/DecSpringBootWeb/HomeController.java
@@ -1,9 +1,6 @@
 package com.raja.DecSpringBootWeb;
 
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
